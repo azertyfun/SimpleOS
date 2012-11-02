deb24a5d051877570facfc7b33c8b0ba6514f2c9
azertyfun <azertyfun_poubelle@hotmail.fr>
2012-11-02 10:05:39 +0100

    Reorganized: all hex files and compile.bat are now in the compiled/ folder

---
+++ compiled/compile.bat
@@ -0,0 +1,10 @@
+echo. >compiled.hex
+type "SimpleOS.hex" >> compiled.hex
+echo. >> compiled.hex
+type "DATA.hex">>compiled.hex
+echo. >> compiled.hex
+type "hardware.hex">>compiled.hex
+echo. >> compiled.hex
+type "programs.hex">>compiled.hex
+
+pause
